8789b8412ea7abbde5b73e0d88fc72f59acbe2b6
Arin Muni Bajracharya <arinbajracharya9@gmail.com>
2025-07-01 06:59:55 +0545

    changed for production

--- server/controller/auth/auth.js
+++ server/controller/auth/auth.js
@@ -60,7 +60,7 @@ exports.register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -88,7 +88,7 @@ exports.login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
